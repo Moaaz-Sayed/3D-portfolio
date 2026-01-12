dc99b233626ab071c2cee3f9d21867e3bae8858e
moaaz_sayed <moaaz4032215@gmail.com>
2026-01-12 16:08:25 +0200

    Initial project setup with React, Router, and pages

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home, About, Projects, Contact } from "./pages";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
